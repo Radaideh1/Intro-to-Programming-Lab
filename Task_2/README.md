7b937202537fee4fc82d82cf6f0d90232b9de041
A_Radaideh <radaideh7484@gmail.com>
2025-05-21 13:47:18 +0300

    add task2 and task3

---
+++ Task_2/README.md
@@ -0,0 +1,21 @@
+# Project Tasks - C++ Programs
+
+This repository contains a collection of simple yet effective C++ programs designed to perform specific computational and input/output tasks. Each program implements a distinct functionality, aiming to enhance programming proficiency and problem-solving skills.
+
+## Tasks Overview
+
+### Compute the Value of F
+A C++ program that computes the value of the following mathematical expression:
+
+
+
+\[ F = x - y + 3 + 4z + 10 \]
+
+
+
+The program prompts the user to enter values for `x`, `y`, and `z`, then calculates and displays the result.
+
+### Reverse Character Sequence
+A C++ program that accepts four characters as input (e.g., `p`, `@`, `+`, `1`) and prints them in reverse order, separated by underscores.
+
+#### Example Output:
---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+
+		int check;
+		cout << "1) F = x-y^3 + 4z + 10 .\n2)Print the entered characters in reverse.\
+\n3)From Celsius to Kelvin." << endl;
+		cin >> check;
+		switch (check)
+		{
+		case 1: {
+
+			int f, x, y, z;
+			cout << "Plese enter the value of x,y,z : ";
+			cin >> x >> y >> z;
+
+			f = x - (y * y * y) + 4 * z + 10;
+			cout << "The value of F = " << f << endl;
+			break;
+		}
+		case 2: {
+			char a, b, c, d;
+			cout << "Entre 4 characters : ";
+			cin >> a >> b >> c >> d;
+
+			cout << d << "_" << c << "_" << b << "_" << a << endl;
+
+			break;
+		}
+		case 3: {
+			float k, c;
+			cout << "Enter a temperature in Celsius : ";
+			cin >> c;
+			k = c + 273.15f;
+			cout << "The temperature in Kelvin is " << k << endl;
+
+			break;
+		}
+		default: {
+			cout << "Error , Enter Number for 1 to 3 pl " << endl;
+			break;
+		}
+		}
+
+
+
+		cout << endl;
+	}
+	
+
+	return 0;
+
+}
